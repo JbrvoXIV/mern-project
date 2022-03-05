2ba443d1213818a1fdc3bfd4edba88389af9bf77
Jordan Bravo <jbravo0423@gmail.com>
2022-03-05 15:12:29 -0500

    added patch, delete request handlers

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -3,41 +3,22 @@ const Users = require('../models/users.model.js');
 // get user depending on request
 const getUsersController = async (req, res) => {
 
-    if(req.query.first && req.query.last) {
-
-        const firstName = req.query.first.charAt(0).toUpperCase() + req.query.first.slice(1);
-        const lastName = req.query.last.charAt(0).toUpperCase() + req.query.last.slice(1);
-        const fullName = `${firstName} ${lastName}`;
-
+    if(res.fullName) {
         try {
-            const requestedUser = await Users.find({ name: fullName });
+            const requestedUser = await Users.find({ name: res.fullName });
             res.json(requestedUser);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
-
     } else {
-
         try {
             const firstUser = await Users.findOne();
             res.json(firstUser);
         } catch (error) {
             res.status(500).json({ success: false, message: error.message });
         }
-        
     }
-
 }
-
-// get a single user
-
-// const getUserController = async (req, res) => {
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
 
 // create a single user
 const createUsersController = async (req, res) => {
@@ -53,4 +34,33 @@ const createUsersController = async (req, res) => {
     }
 }
 
-module.exports = { getUsersController, createUsersController }
+// update a single user
+const updateUserController = async (req, res) => {
+    try {
+        const updatedUser = await Users.findOneAndUpdate(
+            { name: res.fullName }, 
+            { name: req.body.name }, 
+            { new: true }
+        );
+        res.status(201).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+// deletes a single user
+const deleteUserController = async (req, res) => {
+    try {
+        await Users.findOneAndDelete({ name: res.fullName });
+        res.status(200).json({ message: `Succesfully deleted user ${res.fullName}` });
+    } catch (error) {
+        res.status(400).json({ message: `User ${res.fullName} was not found` });
+    }
+}
+
+module.exports = { 
+    getUsersController, 
+    createUsersController, 
+    updateUserController,
+    deleteUserController
+}
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { getUsersController, createUsersController } = require('../controllers/users.controller.js');
 
-router.get('/api', getUsersController);
+const {
+    getUsersController, 
+    createUsersController, 
+    updateUserController, 
+    deleteUserController 
+} = require('../controllers/users.controller.js');
+
+
+const parseName = (req, res, next) => {
+    
+    if(req.query.first && req.query.last) {
+        const firstName = req.query.first.charAt(0).toUpperCase() + req.query.first.slice(1);
+        const lastName = req.query.last.charAt(0).toUpperCase() + req.query.last.slice(1);
+        const fullName = `${firstName} ${lastName}`;
+        
+        res.fullName = fullName;
+    }
+    
+    next();
+}
+
+router.get('/api', parseName, getUsersController);
 
 router.post('/api', createUsersController);
+
+router.patch('/api', parseName, updateUserController);
+
+router.delete('/api', parseName, deleteUserController);
 
 module.exports = router;
